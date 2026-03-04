e23978c2f0916089f90e7d4dd471294f1b0c17c2
Rihards Mantejs <dexit@dyc.lv>
2026-03-04 14:14:46 +0000

    Create base Controller class

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Http\Controllers\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    // Base controller functionality can be implemented here
+}
